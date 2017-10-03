1bfad5e4b2548550be66ff68ddea87e8ed4005d0
seirin29 <31317917+seirin29@users.noreply.github.com>
2017-10-03 22:13:58 +0700

    Level 16 Preparing the Edit view and setting up token

--- resources/views/posts/edit.blade.php
+++ resources/views/posts/edit.blade.php
@@ -5,9 +5,11 @@
 
 	<h1>Edit Post</h1>
 	
-	<form action="/posts" method="post">
-		<input type="text" name="title" />
-		{{csrf_field()}}
+	<form action="/posts/{{$post->id}}" method="post">
+		<input type="hidden" name="_method" value="PUT" />
+		
+		<input type="text" name="title" value="{{$post->title}}" />
+			{{csrf_field()}}
 		<input type="submit" name="submit" />
 	</form>
 	
