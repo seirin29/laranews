858117239f438475c38b84d036ba2590864d0442
seirin29 <31317917+seirin29@users.noreply.github.com>
2017-10-03 23:02:07 +0700

    Level 17 Modifying Create Form with the Form Package

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -5,11 +5,22 @@
 @section('content')
 
 	<!-- <form action="/posts" method="post"> -->
-	{!! Form::open() !!}	
-		<input type="text" name="title"/>
+	{!! Form::open(['method'=>'POST', 'action'=>'PostsController@store']) !!}	
+		
+		<div class="form-group">
+			{!! Form::label('title', 'Title:') !!}
+			{!! Form::text('title', null, ['class'=>'form-control']) !!}
+		</div>
+		
+		<div class="form-group">
+			{!! Form::submit('Create Post', ['class'=>'btn btn-primary']) !!}
+		</div>	
+	
+	{!! Form::close() !!}
+		<!-- <input type="text" name="title"/>
 		{{csrf_field()}}
 		<input type="submit" name="submit" />
-	</form>
+	</form> -->
 	
 @stop
 
