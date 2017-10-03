417408fbd600c802bd3d73ac63564de211bfca74
seirin29 <31317917+seirin29@users.noreply.github.com>
2017-10-03 20:21:58 +0700

    Level 16 Setting the Markup for Create

---
+++ resources/views/posts/create.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.app')
+
+
+
+@section('content')
+
+	<form action="/posts" method="post">
+		<input type="text" name="title"/>
+		<input type="submit" name="submit" />
+	</form>
+	
+@stop
+
+@section('footer')
+
+@stop
